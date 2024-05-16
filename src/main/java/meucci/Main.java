09b8007c10fccd1f5b39735b63c210e40a6634d0
lai2-24 <lai2-24@itismeucci.com>
2024-05-16 13:57:51 +0200

    second commit

--- src/main/java/meucci/Main.java
+++ src/main/java/meucci/Main.java
@@ -2,7 +2,7 @@ package meucci;
 
 public class Main {
     public static void main(String[] args) {
-        MyThread t = new MyThread();
+        MyThread t = new MyThread("Mario");
         t.start();
         for(int i = 0; i < 10; i++){
             System.out.println("Il Main sta contando: " + i);
--- src/main/java/meucci/MyThread.java
+++ src/main/java/meucci/MyThread.java
@@ -1,10 +1,14 @@
 package meucci;
 
 public class MyThread extends Thread{
+    public MyThread(String nome){
+        super(nome);
+    }
+
     @Override
     public void run() {
         for(int i = 0; i < 10; i++){
-            System.out.println("Il Thread sta contando: " + i);
+            System.out.println(this.getName() + " sta contando: " + i);
         }
     }
 }
